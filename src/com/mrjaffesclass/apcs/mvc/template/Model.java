177c60ba5eefc5d764db1850cd8cd5d49815c1e9
srios <srios01@cox.net>
2015-02-11 22:40:46 -0800

    spenser.rios-PDA
    A program to calculate the permissible dating age of a user.

--- src/com/mrjaffesclass/apcs/mvc/template/Model.java
+++ src/com/mrjaffesclass/apcs/mvc/template/Model.java
@@ -2,96 +2,37 @@ package com.mrjaffesclass.apcs.mvc.template;
 
 import com.mrjaffesclass.apcs.messenger.*;
 
-/**
- * The model represents the data that the app uses.
- * @author Roger Jaffe
- * @version 1.0
- */
 public class Model implements MessageHandler {
 
   // Messaging system for the MVC
   private final Messenger mvcMessaging;
-
-  // Model's data variables
-  private int variable1;
-  private int variable2;
-
-  /**
-   * Model constructor: Create the data representation of the program
-   * @param messages Messaging class instantiated by the Controller for 
-   *   local messages between Model, View, and controller
-   */
   public Model(Messenger messages) {
     mvcMessaging = messages;
   }
   
-  /**
-   * Initialize the model here and subscribe to any required messages
-   */
   public void init() {
-    mvcMessaging.subscribe("controller:changeButton", this);
-    setVariable1(10);
-    setVariable2(-10);
+    mvcMessaging.subscribe("view:goButton", this);
   }
   
   @Override
   public void messageHandler(String messageName, Object messagePayload) {
     if (messagePayload != null) {
-      System.out.println("RCV (model): "+messageName+" | "+messagePayload.toString());
+      System.out.println("MSG: received by model: "+messageName+" | "+messagePayload.toString());
     } else {
-      System.out.println("RCV (model): "+messageName+" | No data sent");
+      System.out.println("MSG: received by model: "+messageName+" | No data sent");
+    }  
+        if(messageName.equals("view:goButton"))
+        {
+            mvcMessaging.notify("model:getAges", calcAges(Integer.parseInt((String)messagePayload)));
+        }
     }
-    MessagePayload payload = (MessagePayload)messagePayload;
-    int field = payload.getField();
-    int direction = payload.getDirection();
     
-    if (direction == Constants.UP) {
-      if (field == 1) {
-        setVariable1(getVariable1()+Constants.UP);
-      } else {
-        setVariable2(getVariable2()+Constants.UP);
-      }
-    } else {
-      if (field == 1) {
-        setVariable1(getVariable1()+Constants.DOWN);
-      } else {
-        setVariable2(getVariable2()+Constants.DOWN);
-      }      
+  public int[] calcAges(int age)
+    {
+        int i[] = new int[2];
+        i[0] = (int)Math.ceil((double)age/2+7);
+        i[1] = (int)Math.ceil((double)age*2-14);
+        return i;
     }
-  }
-
-  /**
-   * Getter function for variable 1
-   * @return Value of variable1
-   */
-  public int getVariable1() {
-    return variable1;
-  }
-
-  /**
-   * Setter function for variable 1
-   * @param v New value of variable1
-   */
-  public void setVariable1(int v) {
-    variable1 = v;
-    mvcMessaging.notify("model:variable1Changed", variable1, true);
-  }
   
-  /**
-   * Getter function for variable 1
-   * @return Value of variable2
-   */
-  public int getVariable2() {
-    return variable2;
-  }
-  
-  /**
-   * Setter function for variable 2
-   * @param v New value of variable 2
-   */
-  public void setVariable2(int v) {
-    variable2 = v;
-    mvcMessaging.notify("model:variable2Changed", variable2, true);
-  }
-
 }
